98c46eb02d11930c4447247604186910c455fa22
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-15 01:57:39 +0600

    close countdown display when it comes to 0.

--- script.js
+++ script.js
@@ -117,10 +117,9 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
-
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
-      console.log(display.classList);
+      // console.log(display);
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
